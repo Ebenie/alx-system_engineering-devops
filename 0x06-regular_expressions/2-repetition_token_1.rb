8ec2152dbffc77e48af8306496601c15c351ddef
Ebenie <abrahamavenue163@.com>
2024-02-02 13:27:32 -0800

    Updating

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,19 +1,7 @@
 #!/usr/bin/env ruby
-# This Ruby script checks if a string argument is provided from the command line, defines a regular expression pattern to match "htn" or "htbn" patterns, then matches
-
+#This ruby script find regular expression to check if the argument is provided
 if ARGV.empty?
-  puts "Usage: #{$0} <string>"
   exit 1
 end
-
-pattern = /ht(n|bn)/
-
-input_string = ARGV[0]
-
-match = input_string.match(pattern)
-
-if match
-  puts "Match found: #{match[0]}"
-else
-  puts "No match found"
-end
+pattern = /h(b+|tn)+/
+matches = ARGV[0].scan(pattern)
