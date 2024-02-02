822ab6769cdf0cc4abf86323e52fa8d07a19cf7e
Ebenie <abrahamavenue163@.com>
2024-02-02 13:32:46 -0800

    Updating

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,7 +1,10 @@
 #!/usr/bin/env ruby
 #This ruby script find regular expression to check if the argument is provided
+
 if ARGV.empty?
   exit 1
 end
-pattern = /h(b+|tn)+/
-matches = ARGV[0].scan(pattern)
+regex = /hb+t*n/
+matches = ARGV[0].scan(regex)
+input = ARGV[0]
+puts input.match(regex)
