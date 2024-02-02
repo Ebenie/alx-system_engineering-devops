94aea0d1569a4feb85ae627bcbffe85a7b54e176
Ebenie <abrahamavenue163@.com>
2024-02-02 14:13:32 -0800

    Updating

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,11 +1,8 @@
 #!/usr/bin/env ruby
-# This ruby script read each line from the log file provided as the first argument
+# This rub script extract sender, receiver, and flags using regular expression
 
-File.foreach(ARGV[0]) do |line|
+matches = ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/)
 
-    sender = line.match(/\[from:(?<sender>[\w+\s]+)\]/)[:sender]
-    receiver = line.match(/\[to:(?<receiver>[\+\d]+)\]/)[:receiver]
-    flags = line.match(/\[flags:(?<flags>[\d\-\:]+)\]/)[:flags]
-
-    puts "#{sender},#{receiver},#{flags}"
+matches.each do |match|
+  puts "#{match[0]},#{match[1]},#{match[2]}"
 end
