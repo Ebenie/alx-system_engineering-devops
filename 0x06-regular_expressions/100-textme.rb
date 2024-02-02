599ff990e0cbc03869e2b289f1556ff4ca18d357
Ebenie <abrahamavenue163@.com>
2024-02-02 14:02:49 -0800

    Updating

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,16 +1,11 @@
 #!/usr/bin/env ruby
-# This script defines a method to extract sender, receiver, and flags from log lines
-
-def extract_info(line)
-  sender = line.match(/\[from:(.+?)\]/)[1]
-  receiver = line.match(/\[to:(.+?)\]/)[1]
-  flags = line.match(/\[flags:(.+?)\]/)[1]
-  [sender, receiver, flags]
-end
+# This ruby script read each line from the log file provided as the first argument
 
 File.foreach(ARGV[0]) do |line|
-  if line.include?("Sent SMS") || line.include?("Receive SMS")
-    sender, receiver, flags = extract_info(line)
+
+    sender = line.match(/\[from:(?<sender>[\w+\s]+)\]/)[:sender]
+    receiver = line.match(/\[to:(?<receiver>[\+\d]+)\]/)[:receiver]
+    flags = line.match(/\[flags:(?<flags>[\d\-\:]+)\]/)[:flags]
+
     puts "#{sender},#{receiver},#{flags}"
-  end
 end
