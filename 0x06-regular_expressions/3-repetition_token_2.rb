4346f431c1f82ba674dca3b3042750cc6dd44e61
Ebenie <abrahamavenue163@.com>
2024-02-02 13:16:27 -0800

    Updating

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,18 +1,17 @@
 #!/usr/bin/env ruby
-# This ruby script that accepts a string argument and matches it against a regular expression pattern to find occurrences of "hbtn", "hbttn", "hbtttn", or "hbttttn".
+# This ruby script check if the argument is provided matches.
 
 if ARGV.empty?
   puts "Usage: #{$0} <string>"
   exit 1
 end
 
-pattern = /hb(t{2,5})n/
+pattern = /#{ARGV[0]}/
+testing_string = "hbn,hbtn,hbttn,hbtttn,hbttttn"
+match_string = "hbtn,hbttn,hbtttn,hbttttn"
 
-input_string = ARGV[0]
-
-match = input_string.match(pattern)
-if match
-  puts "Match found: #{match[0]}"
-else
-  puts "No match found"
+matches = testing_string.scan(pattern)
+puts "Matches found:"
+matches.each do |match|
+  puts match
 end
