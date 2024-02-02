fdc98e80e9e426ab5edcd20d5fc489d3164c31b8
Ebenie <abrahamavenue163@.com>
2024-02-02 13:56:13 -0800

    Updating

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,10 +1,7 @@
 #!/usr/bin/env ruby
 # This ruby script check if the argument is provided matches.
 
-if ARGV.empty?
-  puts "Usage: #{$0} <string>"
-  exit 1
-end
-
-pattern = /(hbtn|htn)+/
-matches = ARGV[0].scan(pattern)
+regex = /hbt*n/
+input = ARGV[0]
+matches = input.scan(regex)
+puts matches.join(",")
