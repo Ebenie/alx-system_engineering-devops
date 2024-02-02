65232361951dd84bdcfc81cd6a0abafbd951f130
Ebenie <abrahamavenue163@.com>
2024-02-02 14:16:15 -0800

    Updating

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,8 +1,2 @@
 #!/usr/bin/env ruby
-# This rub script extract sender, receiver, and flags using regular expression
-
-matches = ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/)
-
-matches.each do |match|
-  puts "#{match[0]},#{match[1]},#{match[2]}"
-end
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
