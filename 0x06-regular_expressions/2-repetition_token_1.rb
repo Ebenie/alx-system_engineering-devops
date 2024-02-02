046ebd2c69ea64f61bf489b98dbdcce518b3c999
Ebenie <abrahamavenue163@.com>
2024-02-02 13:49:38 -0800

    Updating

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,17 +1,9 @@
 #!/usr/bin/env ruby
-#This ruby script find regular expression to check if the argument is provided
+#This ruby script regular expression for matching repetition token #1
+regex = /hbt{2,5}n/
 
-def match_regex(argument)
-  pattern = /hbt{2,5}n/
-  if argument.match?(pattern)
-    puts "Match found: #{argument}"
-  else
-    puts "No match found for #{argument}"
-  end
-end
+input = ARGV[0]
 
-if ARGV.length != 1
-  puts "Usage: ruby 2-repetition_token_1.rb <argument>"
-else
-  match_regex(ARGV[0])
-end
+matches = input.scan(regex)
+
+puts matches.join(",")
