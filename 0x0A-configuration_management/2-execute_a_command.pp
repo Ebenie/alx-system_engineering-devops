2529d85f891d89ccfd59e0780f083d11787d92bd
Ebenie <abrahamavenue163@.com>
2024-03-27 07:13:15 -0700

    updated version

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,7 @@
 # Define an exec resource to kill the process named "killmenow"
 
 exec { 'pkill':
-  command   => 'pkill killmenow',
-  path   => '/bin:/usr/bin:/sbin:/usr/sbin',
- 
+  command => 'pkill killmenow',
+  path    => '/bin:/usr/bin:/sbin:/usr/sbin',
 }
 
