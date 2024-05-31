a62ab1b1131f3e12309ec1b13c815abe815cc2ba
Ebenie <124689465+Ebenie@users.noreply.github.com>
2024-05-31 13:55:04 +0300

    Delete 0x1B-web_stack_debugging_4 directory

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++
@@ -1,7 +0,0 @@
-# Puppet manifest to change OS configuration for the holberton user
-
-# Increase the maximum number of open files for the holberton user
-user { 'holberton':
-  max_files => '65536',
-}
-
